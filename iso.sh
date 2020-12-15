ac5a7eacce1684173ce880ee38039793b80fcf60
frogstair <daniladudkin412@gmail.com>
2020-12-15 18:42:10 +0200

    Initial commit

---
+++ iso.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+. ./build.sh
+
+mkdir -p isodir
+mkdir -p isodir/boot
+mkdir -p isodir/boot/grub
+
+cp sysroot/boot/tynvos.kernel isodir/boot/tynvos.kernel
+cat > isodir/boot/grub/grub.cfg << EOF
+menuentry "tynvos" {
+	multiboot /boot/tynvos.kernel
+}
+EOF
+grub-mkrescue -o tynvos.iso isodir
