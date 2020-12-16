51ec84d3049167f3135499aab02fabe294228bd5
frogstair <daniladudkin412@gmail.com>
2020-12-16 20:42:55 +0200

    Add logo

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,5 +4,7 @@
 
 void kernel_main(void) {
 	terminal_initialize();
-	printf("Hello, World!");
+	printf("                              \n            +my.              \n          +mMMy-              \n        +mMMy-                \n      +mMMd:                  \n    +mMMMMMMMmy/`             \n  +NMMmy+//+sdMMNo        -hd:\n`hMMh-        .yMMd`    -hMMm/\nsMMy            oMMh  -hMMm/  \nNMM`             NMM/hMMm/    \nNMM.             NMMMMm/      \noMMh`           sMMMm/        \n sMMm/`       :dMMm/          \n  :dMMMdsooshNMMm/            \n    .+ymNMMNmho-              \n\nWelcome to tynvos\n\n");
+	printf("Terminal initialized, this works :)\n\nMade by frogstair");
+
 }
